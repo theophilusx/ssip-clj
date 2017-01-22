1793270dbfc8950eb0321442f65437a91afd9b06
Tim Cross <theophilusx@gmail.com>
2017-01-23 08:06:47 +1100

    Bumped dependency versions

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [com.taoensso/timbre "4.1.4"]
-                 [org.clojure/core.async "0.2.374"]
-                 [environ "1.0.1"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [com.taoensso/timbre "4.8.0"]
+                 [org.clojure/core.async "0.2.395"]
+                 [environ "1.1.0"]]
 
   :plugins [[lein-environ "1.0.1"]]
 
